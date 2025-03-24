cc0f1ed152a99e0770fd2fcdbb7d9c4bdb382338
Rahmandi Fitra Madenda <99945506+randyisalive@users.noreply.github.com>
2025-03-24 13:09:44 +0700

    update: app collection data

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="*" element={<NotFound />} />
           <Route path="/" element={<Home />} />
         </Routes>
         <Footer />
