781b234bc9ed2047201b82af0856fd84628ae4a6
Rahmandi Fitra Madenda <99945506+randyisalive@users.noreply.github.com>
2025-04-25 08:16:46 +0700

    Update route.js

--- app/api/app_data/route.js
+++ app/api/app_data/route.js
@@ -50,7 +50,7 @@ export async function GET(request) {
         attachment,
       };
     });
-    return NextResponse.json(combinedData);
+    return NextResponse.json({ data: combinedData });
   }
-  return NextResponse.json(data.data);
+  return NextResponse.json({ data: data.data });
 }
